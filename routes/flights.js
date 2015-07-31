7fcf080d1fd01cdabf3c3741b1179a8b4e55f460
hadooplatheesh <latheesh@gmail.com>
2015-07-31 14:19:33 +0530

    Initial Module structure
    This is the base structure for the flight module

---
+++ routes/flights.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+var Schema = mongoose.Schema;
+var flightschema = new Schema({
+    flightname: String,
+    flightId: String,
+    flightOriginAirport: String
+});
+
+// Mongoose Model definition
+var flightmodel = mongoose.model('flights', flightschema);
+
+var conn = mongoose.connect("mongodb://192.168.11.4:27017/FlightsDB", function(err, db) {
+    if (err) {
+        console.log(err);
+    }
+});
+exports.insert = function(req, res){
+
+	// Mongoose Schema definition
+	
+	// Bootstrap express
+	var flight = new flightmodel();
+	flight.flightname = 'Virgin Atlantic';
+	flight.flightId = 'V456890';
+	flight.flightOriginAirport = 'SFO';
+	
+	flight.save(function(err, user_Saved){
+	    if(err){
+		        throw err;
+		        console.log(err);
+		    }else{
+		        console.log('saved!');
+		    }
+		});
+}
+
+
+exports.show = function(req, res){
+	flightmodel.find({}, function (err, docs) {
+	        res.json(docs);
+	 });
+}
+
